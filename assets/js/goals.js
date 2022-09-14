1f73eaec93c6ab7db0c7f56292f89c81743eaac5
Tobias Aagaard <Tobias909@live.dk>
2022-09-14 14:46:39 +0200

    Min fetch

--- assets/js/goals.js
+++ assets/js/goals.js
@@ -4,41 +4,36 @@ const getApiData = () => {
   const apiEndpoint = `https://api.mediehuset.net/sdg/goals`;
 
   fetch(apiEndpoint)
-    /** fetch (Promise) at forsøge hente data fra et api endpoint */
-    .then((response) => {
-      /** if promise is resolved then */
-      /** in the response we can find the ok to be true or false*/
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      console.log(data.items);
-      apiData.push(...data.items);
-    })
-    .catch((error) => {
-      /** if promise is not resolved catch */
-      console.error(error);
-    })
-    .finally(() => {
-      /** if everything is a success run this */
-      renderContent();
-    });
+  .then((Response) => {
+    if (Response.ok) {
+      return Response.json();
+    }
+  })
+  .then((data) => {
+    console.log(data.items);
+    apiData.push(...data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    renderContent();
+  });
 };
 
 let renderContent = () => {
   apiData.map((data) => createElements(data));
-};
+}
 
 const createElements = (data) => {
   document.getElementById("mother").innerHTML += `
-        <figure class="card" style="background-color:#${data.color};" >
-          
-            <div class="container">
-            <img src='data:image/svg+xml; utf8,${data.icon}'  alt="icon" >
-              <h4><b>${data.title}</b></h4>              
-            </div>
-        </figure>`;
-};
+    <figure class="card" style="background-color: #${data.color};">
+      <div class="container">
+        <img src='data:image/svg+xml; utf8,${data.icon}' alt="icon">
+        <h4>${data.title}</h4>
+        <p>${data.byline}</p>
+      </div>
+    </figure>`;
+}
 
 getApiData();
